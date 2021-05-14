2fef480e3aafa4d8521e450bc8dad56f43704ef9
sobhankazemi <78590935+sobhankazemi@users.noreply.github.com>
2021-05-14 22:54:50 +0430

    Game_AP.cpp updated
    the problem of repeating same question fixed

--- Game_AP/Game_AP/Game_AP.cpp
+++ Game_AP/Game_AP/Game_AP.cpp
@@ -5,8 +5,15 @@
 using namespace std;
 int main()
 {
+	const int count_q = 4;
 	srand(time(0));
-	int index = rand() % 4;
+	bool q_list[count_q] = { true };
+	int index;
+	do
+	{
+		index = rand() % count_q;
+	} while (q_list[index] == false);
+	q_list[index] = true;
 	Question q(index);
 
 }
