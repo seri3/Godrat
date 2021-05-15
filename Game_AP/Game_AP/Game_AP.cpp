2fafca048201abafeb49d875d874797269daf2d4
sobhankazemi <78590935+sobhankazemi@users.noreply.github.com>
2021-05-15 18:27:43 +0430

    update the main part
    the problem of repeating same question finally fixed

--- Game_AP/Game_AP/Game_AP.cpp
+++ Game_AP/Game_AP/Game_AP.cpp
@@ -1,20 +1,18 @@
 #include "Question.h"
+#include"Answer.h"
 #include <iostream>
 #include<ctime>
 #include<cstdlib>
+#include<vector>
+#include<algorithm>
 using namespace std;
 int main()
 {
-	const int count_q = 4;
+	vector<int> indexs{ 0,1,2,3 };
 	srand(time(0));
-	bool q_list[count_q] = { true };
-	int index;
-	do
-	{
-		index = rand() % count_q;
-	} while (q_list[index] == false);
-	q_list[index] = true;
-	Question q(index);
-
+	int index = rand() % indexs.size();
+	Question q(indexs[index]);
+	Answer ans(indexs[index]);
+	indexs.erase(indexs.begin() + index);
 }
 
