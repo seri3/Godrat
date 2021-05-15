dcb3d52a7deb53daac641d5bdc44a30474054b2d
sobhankazemi <78590935+sobhankazemi@users.noreply.github.com>
2021-05-15 23:41:41 +0430

    state class some how completed

--- Game_AP/Game_AP/States.cpp
+++ Game_AP/Game_AP/States.cpp
@@ -1,1 +1,28 @@
 #include "States.h"
+#include<iostream>
+#include<fstream>
+using namespace std;
+States::States(int index, int answer_option)
+{
+	ifstream input;
+	switch (answer_option)
+	{
+	case 1:
+	{
+		input.open("../States_1.txt", ios::in);
+		break;
+	}
+	case 2:
+	{
+		input.open("../States_2.txt", ios::in);
+		break;
+	}
+	}
+	if (!input)
+	{
+		cerr << "file hasnt opened yet" << endl;
+	}
+	input.seekg(index * 8 + index);
+	input >> religion >> satisfaction >> army >> money;
+	cout << religion << '\t' << satisfaction << '\t' << army << '\t' << money << endl;
+}
--- Game_AP/Game_AP/States.h
+++ Game_AP/Game_AP/States.h
@@ -1,5 +1,9 @@
 #pragma once
 class States
 {
+private:
+	int money, satisfaction, religion, army;
+public:
+	States(int,int);
 };
 
